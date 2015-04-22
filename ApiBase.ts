ffd02910b0c26ee55b1c0a88afe1667598ecad1d
GuilhermeBernal <dev@lbguilherme.com>
2015-04-22 00:42:03 -0300

    Simplify error handling

--- ApiBase.ts
+++ ApiBase.ts
@@ -5,8 +5,8 @@ import Http = require("http");
 class ApiBase {
 	[endpoint: string]: any;
 
-	fail(resp : Http.ServerResponse, message : string) {
-		resp.statusCode = 400;
+	fail(resp : Http.ServerResponse, status : number, message : string) {
+		resp.statusCode = status;
 		resp.write(JSON.stringify({error: message}));
 		resp.end();
 	}
--- ApiV0.ts
+++ ApiV0.ts
@@ -6,19 +6,27 @@ import ApiBase = require("./ApiBase");
 
 class ApiV0 extends ApiBase {
 
+	// Checks for GraphAPI errors. If an error has been handled, returns true
+	handleGraphError(resp : Http.ServerResponse, error : Graph.Error) : boolean {
+		if (!error) return false;
+
+		if (error.type == "OAuthException")
+			this.fail(resp, 401, error.message);
+		else
+			this.fail(resp, 400, error.message);
+
+		return true;
+	}
+
 	_notify_login(params : any, resp : Http.ServerResponse) {
 		var token : string = params.token;
 
-		if (typeof token != "string")
-			return this.fail(resp, "invalid token");
-
-		Graph.get("me", {access_token: token}, (err : Graph.Error, user : Graph.User) => {
-			if (err !== null)
-				return this.fail(resp, err.message);
+		Graph.get("me", {access_token: token}, function(err : Graph.Error, user : Graph.User) {
+			if (this.handleGraphError(resp, err)) return;
 
 			resp.write("Your id is '" + user.id + "'.");
 			resp.end();
-		});
+		}.bind(this));
 	}
 
 }
