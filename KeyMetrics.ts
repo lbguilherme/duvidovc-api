d1c72119bcbf3f60f7f98e2362e4c831e712a0f4
GuilhermeBernal <dev@lbguilherme.com>
2015-04-22 10:15:41 -0300

    Add git related actions to Keymetrics

--- KeyMetrics.ts
+++ KeyMetrics.ts
@@ -4,6 +4,18 @@ import ChildProcess = require("child_process");
 export function init() {
 	Pmx.init();
 
+	Pmx.action("git status", function(reply) {
+		ChildProcess.exec("git status", function callback(error : Error, stdout : Buffer, stderr : Buffer){
+			reply({error: error, stdout: stdout.toString(), stderr: stderr.toString()});
+		})
+	});
+
+	Pmx.action("git pull", function(reply) {
+		ChildProcess.exec("git pull", function callback(error : Error, stdout : Buffer, stderr : Buffer){
+			reply({error: error, stdout: stdout.toString(), stderr: stderr.toString()});
+		})
+	});
+
 	Pmx.action("build", function(reply) {
 		ChildProcess.exec("make js/main.js", function callback(error : Error, stdout : Buffer, stderr : Buffer){
 			reply({error: error, stdout: stdout.toString(), stderr: stderr.toString()});
