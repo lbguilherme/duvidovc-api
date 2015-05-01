14d57681de9d9410dc57f602c3ce295e4534a153
Guilherme Bernal <dev@lbguilherme.com>
2015-05-01 17:35:55 -0300

    Remove unused function ApiBase.fail

--- src/ApiBase.ts
+++ src/ApiBase.ts
@@ -4,10 +4,4 @@ import Http = require("http");
 
 class ApiBase {
 	[endpoint: string]: any;
-
-	fail(resp : Http.ServerResponse, status : number, message : string) {
-		resp.statusCode = status;
-		resp.write(JSON.stringify({error: message}));
-		resp.end();
-	}
 }
