264741ccd680552699956f286692e670cdcebd81
Guilherme Bernal <dev@lbguilherme.com>
2015-05-17 15:52:01 -0300

    Use 401 status code when the access token is invalid

--- src/ApiBase.ts
+++ src/ApiBase.ts
@@ -6,7 +6,11 @@ class ApiBase {
 	[endpoint: string]: (params : any, resp : Http.ServerResponse) => void;
 	
 	fail(message : string, resp : Http.ServerResponse) {
-		resp.statusCode = 500;
+		if (message.indexOf("OAuthException") != -1)
+			resp.statusCode = 401;
+		else
+			resp.statusCode = 500;
+		
 		resp.setHeader("Content-Type", "text/plain");
 		resp.write("Error: " + message);
 		resp.end();
