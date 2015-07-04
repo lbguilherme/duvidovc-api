0d32a3c2e1c391348eb7dbadc0b935b862aecc2e
Guilherme Bernal <dev@lbguilherme.com>
2015-07-04 10:43:24 -0300

    Remove deprecated Utility.doForAll

--- src/Utility.ts
+++ src/Utility.ts
@@ -4,17 +4,6 @@ import Http = require("http");
 import Bluebird = require("bluebird");
 import await = require("asyncawait/await");
 
-export function doForAll(times : number, action : (i : number, done : () => void) => void, allDone : () => void) {
-	var dones = 0;
-	for (var i = 0; i < times; ++i) {
-		action(i, () => {
-			dones += 1;
-			if (dones == times)
-				allDone();
-		});
-	}
-}
-
 export function readAll(msg : Http.IncomingMessage) {
 	return await(new Bluebird.Promise((resolve) => {
 		var contents = "";
